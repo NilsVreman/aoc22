cbeca0f0830c0ad37d62b2650da19b303ca83ff6
NilsVreman <nils.vreman@gmail.com>
2022-12-02 08:24:57 +0100

    Added day02 to repo

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,1 +1,76 @@
-use parser::Content;
+enum Shape {
+    R,
+    P,
+    S,
+}
+
+enum Outcome {
+    L,
+    W,
+    D,
+}
+
+impl Outcome {
+    pub fn value(&self) -> u32 {
+        match self {
+            Outcome::L => 0,
+            Outcome::W => 6,
+            Outcome::D => 3,
+        }
+    }
+}
+
+impl Shape {
+
+    pub fn build(s: &str) -> Shape {
+        match s {
+            "A" | "X" => Shape::R,
+            "B" | "Y" => Shape::P,
+            "C" | "Z" => Shape::S,
+            _ => panic!("No such str representing a shape"),
+        }
+    }
+
+    pub fn build_from_answer(s: &str, other: &Shape) -> Shape {
+        match (other, s) {
+            (Shape::R, "Y") | (Shape::P, "X") | (Shape::S, "Z") => Shape::R,
+            (Shape::R, "Z") | (Shape::P, "Y") | (Shape::S, "X") => Shape::P,
+            (Shape::R, "X") | (Shape::P, "Z") | (Shape::S, "Y") => Shape::S,
+            _ => panic!("No such str representing a shape"),
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        match self {
+            Shape::R => 1,
+            Shape::P => 2,
+            Shape::S => 3,
+        }
+    }
+
+    pub fn game(&self, other: &Shape) -> Outcome {
+        match (self, other) {
+            (Shape::R, Shape::R) | (Shape::P, Shape::P) | (Shape::S, Shape::S) => Outcome::D,
+            (Shape::R, Shape::S) | (Shape::P, Shape::R) | (Shape::S, Shape::P) => Outcome::W,
+            (Shape::R, Shape::P) | (Shape::P, Shape::S) | (Shape::S, Shape::R) => Outcome::L,
+        }
+    }
+
+    pub fn points(&self, other: &Shape) -> u32 {
+        self.game(&other).value() + self.value()
+    }
+}
+
+pub fn game(arg_list: &Vec<&str>) -> u32 {
+    let other_choice = Shape::build(arg_list[0]);
+    let my_choice = Shape::build(arg_list[1]);
+
+    my_choice.points(&other_choice)
+}
+
+pub fn game_from_answer(arg_list: &Vec<&str>) -> u32 {
+    let other_choice = Shape::build(arg_list[0]);
+    let my_choice = Shape::build_from_answer(arg_list[1], &other_choice);
+
+    my_choice.points(&other_choice)
+}
--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,11 +1,48 @@
-use parser::Content;
+use parser;
 
 pub fn main() {
-    
+    let c = parser::Content::read_file(&"input.txt").expect("No such file found");
+    let ps = parser::Parser::build("{} {}", "{}");
+    let res: u32 = c.content
+        .lines()
+        .map(|x| day02::game(&ps.parse(x).expect("Invalid parsing")))
+        .sum();
+    println!("Res exc 1: {}", res);
+
+    let res: u32 = c.content
+        .lines()
+        .map(|x| day02::game_from_answer(&ps.parse(x).expect("Invalid parsing")))
+        .sum();
+    println!("Res exc 2: {}", res);
 }
 
 #[cfg(test)]
 mod tests {
     use parser;
+    use day02;
+
+    #[test]
+    fn t1_test() {
+        let input = "A Y
+B X
+C Z";
+        let ps = parser::Parser::build("{} {}", "{}");
+        let res: Vec<u32> = input.lines().map(|x| day02::game(&ps.parse(x).expect("Invalid parsing"))).collect();
+        let cmp: Vec<u32> = vec![8, 1, 6];
+
+        assert_eq!(res, cmp)
+    }
+
+    #[test]
+    fn t2_test() {
+        let input = "A Y
+B X
+C Z";
+        let ps = parser::Parser::build("{} {}", "{}");
+        let res: Vec<u32> = input.lines().map(|x| day02::game_from_answer(&ps.parse(x).expect("Invalid parsing"))).collect();
+        let cmp: Vec<u32> = vec![4, 1, 7];
+
+        assert_eq!(res, cmp)
+    }
 
 }
