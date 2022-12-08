d63a5a2f3c2af5b44f412fbb2367c78fb737f291
NilsVreman <nils.vreman@gmail.com>
2022-12-08 21:47:41 +0100

    Pretty nice improvement to code

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -11,127 +11,58 @@ impl Tree {
     }
 }
 
-pub struct Line {
-    line: Vec<Tree>,
-}
-
-impl Line {
-    pub fn new(l: &str) -> Line {
-        Line { line: l.chars().map(|x| Tree::new(&x)).collect() }
-    }
-
-    /// Visible on left side
-    fn l_visible(&self) -> Vec<bool> {
-        self.line.iter()
-            .enumerate()
-            .map(|(i,x)| self.line.iter()
-                 .take(i)
-                 .find(|other| x.height <= other.height)
-                 .is_none())
-             .collect()
-    }
-
-    /// Visible on right side
-    fn r_visible(&self) -> Vec<bool> {
-        self.line.iter()
-            .rev()
-            .enumerate()
-            .map(|(i,x)| self.line.iter()
-                 .rev()
-                 .take(i)
-                 .find(|other| x.height <= other.height)
-                 .is_none())
-             .rev()
-             .collect()
-    }
-
-    /// Total visible horizontally
-    fn h_visible(&self) -> Vec<bool> {
-        self.l_visible().iter().zip(&self.r_visible()).map(|(l, r)| *l || *r).collect()
-    }
-
-    /// Total number of visible horizontally
-    fn nbr_h_visible(&self) -> u32 {
-        self.h_visible().iter()
-            .filter(|&x| *x)
-            .count() as u32
-    }
-
-    /// Total view horizontally returned as tuple (left, right)
-    fn h_visible_from_tree(&self, c: usize) -> (u32, u32) {
-        let r = self.line.iter()
-            .skip(c+1)
-            .position(|other| self.line[c].height <= other.height)
-            .unwrap_or(self.line.len()-2-c)
-            + 1;
-
-        let l = self.line.iter()
-            .take(c)
-            .rev()
-            .position(|other| self.line[c].height <= other.height)
-            .unwrap_or(c-1)
-            + 1;
-        (l as u32, r as u32)
-    }
-}
-
 pub struct Forest {
-    grid: Vec<Line>,
+    grid: Vec<Vec<Tree>>,
 }
 
 impl Forest {
     pub fn new(c: &parser::Content) -> Forest {
-        Forest { grid: c.content.lines().map(|x| Line::new(&x)).collect() }
+        Forest { grid: c.content.lines()
+            .map(|x| x.chars()
+                 .map(|y| Tree { height: y.to_digit(10).unwrap() })
+                 .collect())
+            .collect() }
     }
 
-    /// Visible on lower (down) side
-    fn d_visible(&self, col: usize) -> Vec<bool> {
-        self.grid.iter()
-            .enumerate()
-            .map(|(i,x)| self.grid.iter()
-                 .take(i)
-                 .find(|other| x.line[col].height <= other.line[col].height)
-                 .is_none())
-             .collect()
-    }
-
-    /// Visible on upper side
-    fn u_visible(&self, col: usize) -> Vec<bool> {
-        self.grid.iter()
+    fn is_visible(&self, row: usize, col: usize) -> bool {
+        if row == 0 || row == self.grid.len()-1 || col == 0 || col == self.grid[0].len()-1 {
+            return true
+        }
+        // from left
+        let l = self.grid[row].iter()
+            .take(col)
+            .find(|x| self.grid[row][col].height <= x.height)
+            .is_none();
+        // from right
+        let r = self.grid[row].iter()
+            .skip(col+1)
             .rev()
-            .enumerate()
-            .map(|(i,x)| self.grid.iter()
-                 .rev()
-                 .take(i)
-                 .find(|other| x.line[col].height <= other.line[col].height)
-                 .is_none())
-             .rev()
-             .collect()
-    }
+            .find(|x| self.grid[row][col].height <= x.height)
+            .is_none();
+        // from up to down
+        let d = self.grid.iter()
+            .take(row)
+            .find(|x| self.grid[row][col].height <= x[col].height)
+            .is_none();
+        // from down to up
+        let u = self.grid.iter()
+            .skip(row+1)
+            .rev()
+            .find(|x| self.grid[row][col].height <= x[col].height)
+            .is_none();
 
-    /// Total visible vertically for column col
-    fn v_visible(&self, col: usize) -> Vec<bool> {
-        self.d_visible(col).iter()
-            .zip(&self.u_visible(col))
-            .map(|(l, r)| *l || *r)
-            .collect()
+        l || r || d || u
     }
 
     /// Map of all visible trees as seen from outside
     pub fn visible(&self) -> HashMap<(usize, usize), bool> {
         let mut map: HashMap<(usize, usize), bool> = HashMap::new();
         let nrows = self.grid.len();
-        let ncols = self.grid[0].line.len();
-
-        let rows: Vec<Vec<bool>> = self.grid.iter()
-            .map(|x| x.h_visible())
-            .collect();
-        let cols: Vec<Vec<bool>> = (0..ncols).map(|x| self.v_visible(x))
-            .collect();
+        let ncols = self.grid[0].len();
 
         for r in 0..nrows {
             for c in 0..ncols {
-                map.insert((r,c), rows[r][c] || cols[c][r]);
+                map.insert((r,c), self.is_visible(r, c));
             }
         }
         map
@@ -144,41 +75,56 @@ impl Forest {
             .count() as u32
     }
 
-    /// Total view vertically returned as tuple (left, right)
-    fn v_visible_from_tree(&self, r: usize, c: usize) -> (u32, u32) {
-        let d = self.grid.iter()
-            .skip(r+1)
-            .position(|other| self.grid[r].line[c].height <= other.line[c].height)
-            .unwrap_or(self.grid.len()-2-r)
-            + 1;
-
-        let u = self.grid.iter()
-            .take(r)
+    fn tree_view(&self, row: usize, col: usize) -> u32 {
+        if row == 0 || row == self.grid.len()-1 || col == 0 || col == self.grid[0].len()-1 {
+            return 0
+        }
+        // to left
+        let l = self.grid[row].iter()
+            .take(col)
             .rev()
-            .position(|other| self.grid[r].line[c].height <= other.line[c].height)
-            .unwrap_or(r-1)
+            .position(|x| self.grid[row][col].height <= x.height)
+            .unwrap_or(col-1)
             + 1;
-        (d as u32, u as u32)
+        // to right
+        let r = self.grid[row].iter()
+            .skip(col+1)
+            .position(|x| self.grid[row][col].height <= x.height)
+            .unwrap_or(self.grid[row].len() - col - 2)
+            + 1;
+        // downward
+        let d = self.grid.iter()
+            .skip(row+1)
+            .position(|x| self.grid[row][col].height <= x[col].height)
+            .unwrap_or(self.grid.len() - row - 2)
+            + 1;
+        // upwards
+        let u = self.grid.iter()
+            .take(row)
+            .rev()
+            .position(|x| self.grid[row][col].height <= x[col].height)
+            .unwrap_or(row-1)
+            + 1;
+
+        (l * r * d * u) as u32
     }
 
-    /// Score from tree at position (row, col)
-    pub fn visible_from_tree(&self, row: usize, col:usize) -> u32 {
-        let (l, r) = self.grid[row].h_visible_from_tree(col); 
-        let (d, u) = self.v_visible_from_tree(row, col);
-        l*r*d*u
-    }
+    /// Map of all visible trees as seen from outside
+    pub fn view(&self) -> HashMap<(usize, usize), u32> {
+        let mut map: HashMap<(usize, usize), u32> = HashMap::new();
+        let nrows = self.grid.len();
+        let ncols = self.grid[0].len();
 
-    /// Best Score from a tree
-    pub fn best_view_from_tree(&self) -> u32 {
-        let mut m = 0;
-        let mut max = 0;
-        for r in 1..self.grid.len()-1 {
-            for c in 1..self.grid[0].line.len()-1 {
-                m = self.visible_from_tree(r, c);
-                if m > max { max = m; }
+        for r in 0..nrows {
+            for c in 0..ncols {
+                map.insert((r,c), self.tree_view(r, c));
             }
         }
-        max
+        map
+    }
+
+    pub fn best_view(&self) -> u32 {
+        *self.view().values().max().expect("Undescribed error")
     }
 }
 
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -4,7 +4,7 @@ pub fn main() {
     let c = parser::Content::read_file(&"input.txt").expect("Input file not found!");
     let forest = day08::Forest::new(&c);
     println!("Part a: {}", forest.nbr_visible());
-    println!("Part b: {}", forest.best_view_from_tree());
+    //println!("Part b: {}", forest.best_view_from_tree());
 }
 
 #[cfg(test)]
@@ -14,7 +14,7 @@ mod tests {
 
 
     #[test]
-    fn t8_test() {
+    fn t1_test() {
         let c = parser::Content::read(&"30373
 25512
 65332
@@ -25,57 +25,13 @@ mod tests {
     }
 
     #[test]
-    fn t9_test() {
+    fn t2_test() {
         let c = parser::Content::read(&"30373
 25512
 65332
 33549
 35390");
         let forest = day08::Forest::new(&c);
-        assert_eq!(forest.nbr_visible(), 21)
-    }
-
-    #[test]
-    fn t10_test() {
-        let c = parser::Content::read(&"30373
-25512
-65332
-33549
-35390");
-        let forest = day08::Forest::new(&c);
-        assert_eq!(forest.visible_from_tree(1 as usize, 2 as usize), 4)
-    }
-
-    #[test]
-    fn t11_test() {
-        let c = parser::Content::read(&"30373
-25512
-65332
-33549
-35390");
-        let forest = day08::Forest::new(&c);
-        assert_eq!(forest.visible_from_tree(3 as usize, 2 as usize), 8)
-    }
-
-    #[test]
-    fn t12_test() {
-        let c = parser::Content::read(&"30373
-25512
-65332
-33549
-35390");
-        let forest = day08::Forest::new(&c);
-        assert_eq!(forest.visible_from_tree(1 as usize, 1 as usize), 1)
-    }
-
-    #[test]
-    fn t13_test() {
-        let c = parser::Content::read(&"30373
-25512
-65332
-33549
-35390");
-        let forest = day08::Forest::new(&c);
-        assert_eq!(forest.best_view_from_tree(), 8)
+        assert_eq!(forest.best_view(), 8)
     }
 }
